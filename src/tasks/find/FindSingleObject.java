8f80846e602b99fa1c0308482b2e83e657524f82
Jakub_Rzeznik <Jakub_Rzeznik@EPPLKRAW0280.budapest.epam.com>
2016-07-26 21:58:18 +0200

    task for today

---
+++ src/tasks/find/FindSingleObject.java
@@ -0,0 +1,14 @@
+package tasks.find;
+
+import java.util.Collection;
+
+public class FindSingleObject {
+
+	//You have a collection of paired integers. All except for one. Your task is to find that single one which does not have a pair
+	//e.g {2,4,2,3,4} => 3
+	
+	public int findSingle(Collection<Integer> col) {
+		
+	}
+
+}
